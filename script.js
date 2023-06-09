ed757b8032314409b7df0c201455fb949a0fe643
sudharsan0003 <sanmarly04@gmail.com>
2023-06-09 06:33:46 +0530

    final

--- script.js
+++ script.js
@@ -32,7 +32,25 @@ if (user >= 1 && user <= 2 || user === 12) {
 } else {
   month = "Invalid month";
 }
-console.log("The Season are:", month); 
+console.log("The Season are:", month);
+
+// 4. Write a program that generates and prints all prime numbers between 1 and 100.
+function isPrime(num) {
+  for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+}
+
+console.log("Prime Numbers between 1 and 100:");
+for (let i = 1; i <= 100; i++) {
+  if (isPrime(i)) {
+    console.log(i);
+  }
+}
+
 
 // 7. Create a function called reverseArray that takes an array as a parameter and returns a new array with the elements in reverse order. 
 
@@ -43,9 +61,30 @@ console.log(colors.reverse());
 
 //  8. Given an array numbers with the values [1, 2, 3, 4, 5], use a for loop to double each number in the array. Print the modified array. 
 
-const num = [1, 2, 3, 4, 5];
+const val = [1, 2, 3, 4, 5];
 
-for (let i = 0; i < num.length; i++) {
-  num[i] *= 2;
+for (let i = 0; i < val.length; i++) {
+  val[i] *= 2;
 }
-console.log("Modified array:", num);
+console.log("Modified arrays are:", val);
+
+// 9. Write a function named flattenArray that takes a nested array as a parameter and returns a new array with all the elements flattened into a single level.
+function flattenArray(nestedArray) {
+  return nestedArray.flat();
+}
+
+const nestedArray = ['a', ['b', ['c', 'd'], 'e']];
+const flattenedArray = flattenArray(nestedArray);
+console.log("Flattened Array:", flattenedArray);
+
+
+// 11. Create an object called "game" with properties for "score" and "updateScore" method that updates the score based on the given points.
+const game = {
+  score: 0,
+  updateScore: function (points) {
+    this.score += points;
+  },
+};
+
+game.updateScore(4);
+console.log("Score:", game.score);
